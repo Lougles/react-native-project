1f787e50ff3a545381a1bafb92315bdbf20c16ff
vovacelidze <louglescom@gmail.com>
2023-01-09 20:56:46 +0200

    added two view blocks, styled them

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState("");
+  const handleInput = (text) => {
+    setText(text)
+  }
   return (
-    <View style={styles.container}>
-      <Text>Hi, this is my first hw!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.title}>Hi, this is my first hw!</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.inputText} placeholder="Type text" value={text} onChangeText={handleInput}/>
+      </View>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 0,
+    marginTop: 24,
+    padding: 24,
+    backgroundColor: "#eaeaea",
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#FF0000'
   },
+  inputContainer: {
+    padding: 24,
+    backgroundColor: "#eaeaea",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: '#FF0000'
+  },
+  inputText: {
+    borderWidth: 1,
+    borderColor: "#FF0000",
+    // padding: 25,
+    height: 30,
+    width: 150,
+  },
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: "#20232a",
+    borderRadius: 6,
+    backgroundColor: "#61dafb",
+    color: "#20232a",
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold"
+  }
 });
